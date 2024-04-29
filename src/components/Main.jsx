f52751e7ca77bf16d959a69725b23e4426d3e8f2
Elvin Cabbarli <87928006+elvincabbarli@users.noreply.github.com>
2024-04-29 11:42:04 +0400

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,20 +20,18 @@ const Main = () => {
       <div>
         <ul className="main-posts">
           {postsAll.map((post, index) => (
-           <Link to=`/post/${post.id}` >
+            <Link to={`/post/${post.id}`}>
               <li className="main-page-posts" key={index}>
-              <h3>{post.heading}</h3>
-              <div dangerouslySetInnerHTML={{ __html: post.content }} />
-              <p>Category: {post.category_name}</p>
-              <div className="action-btns">
-                <button>
-                  <Link to=`/post/${post.id}` >
-                    Comment
-                  </Link>
-                </button>
-                <button>Like</button>
-                <button>Share</button>
-              </div>
+                <h3>{post.heading}</h3>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                <p>Category: {post.category_name}</p>
+                <div className="action-btns">
+                  <button>
+                    <Link to={`/post/${post.id}`}>Comment</Link>
+                  </button>
+                  <button>Like</button>
+                  <button>Share</button>
+                </div>
               </li>
             </Link>
           ))}

008aa7c71a84084451f59f975fbbbed2f61dcbbc
Elvin <cabbarli.elvin75@gmail.com>
2024-05-08 00:00:01 +0400

    d

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllPosts } from "../store/postSlice";
 import { Link } from "react-router-dom";
-import Search from "./Search";
+import axios from "axios";
 
 const Main = () => {
   const dispatch = useDispatch();
   const { postsAll } = useSelector((state) => state.post);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    setSearchResults([])
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`https://fast-quora.onrender.com/search?key=${searchTerm}`);
+      setSearchResults(response.data); // Assuming the response is an array of search results
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchAllPosts());
@@ -16,18 +32,27 @@ const Main = () => {
   return (
     <div>
       <h2>All Posts</h2>
-      <Search />
-      <div>
-        <ul className="main-posts">
-          {postsAll.map((post, index) => (
-            <Link className="main-page-posts" key={post.id} to={`/post/${post.id}`}>
+      <div className="search-container">
+        <form className='search-form' onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleChange}
+            placeholder="Post Axtar..."
+            className="search-input"
+          />
+          <button type="submit" className="search-button">Axtar</button>
+        </form>
+        <div className="main-posts">
+          {searchResults.map((result, index) => (
+            <Link className="main-page-posts" key={result.id} to={`/post/${result.id}`}>
               <li key={index}>
-                <h3>{post.heading}</h3>
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
-                <p>Category: {post.category_name}</p>
+                <h3>{result.heading}</h3>
+                <div dangerouslySetInnerHTML={{ __html: result.content }} />
+                <p>Category: {result.category_name}</p>
                 <div className="action-btns">
                   <button>
-                    <Link to={`/post/${post.id}`}>Comment</Link>
+                    <Link to={`/post/${result.id}`}>Comment</Link>
                   </button>
                   <button>Like</button>
                   <button>Share</button>
@@ -35,6 +60,28 @@ const Main = () => {
               </li>
             </Link>
           ))}
+        </div>
+      </div>
+      <div>
+        <ul className="main-posts">
+          {
+            searchResults.length > 0 ? null : postsAll.map((post, index) => (
+              <Link className="main-page-posts" key={post.id} to={`/post/${post.id}`}>
+                <li key={index}>
+                  <h3>{post.heading}</h3>
+                  <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                  <p>Category: {post.category_name}</p>
+                  <div className="action-btns">
+                    <button>
+                      <Link to={`/post/${post.id}`}>Comment</Link>
+                    </button>
+                    <button>Like</button>
+                    <button>Share</button>
+                  </div>
+                </li>
+              </Link>
+            ))
+          }
         </ul>
       </div>
     </div>
